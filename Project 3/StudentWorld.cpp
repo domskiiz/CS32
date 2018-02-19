71ae8428286b2e075ae399e38111602b9935f0e2
Alissa Niewiadomski <alissaaadomski@gmail.com>
2018-02-18 23:31:15 -0800

    project3 init

---
+++ Project 3/StudentWorld.cpp
@@ -0,0 +1,33 @@
+#include "StudentWorld.h"
+#include "GameConstants.h"
+#include <string>
+using namespace std;
+
+GameWorld* createStudentWorld(string assetDir)
+{
+	return new StudentWorld(assetDir);
+}
+
+// Students:  Add code to this file, StudentWorld.h, Actor.h and Actor.cpp
+
+StudentWorld::StudentWorld(string assetDir)
+: GameWorld(assetDir)
+{
+}
+
+int StudentWorld::init()
+{
+    return GWSTATUS_CONTINUE_GAME;
+}
+
+int StudentWorld::move()
+{
+    // This code is here merely to allow the game to build, run, and terminate after you hit enter.
+    // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+    decLives();
+    return GWSTATUS_PLAYER_DIED;
+}
+
+void StudentWorld::cleanUp()
+{
+}
---
+++ Project 3/StudentWorld.h
@@ -0,0 +1,20 @@
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include <string>
+
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class StudentWorld : public GameWorld
+{
+public:
+    StudentWorld(std::string assetDir);
+    virtual int init();
+    virtual int move();
+    virtual void cleanUp();
+
+private:
+};
+
+#endif // STUDENTWORLD_H_
